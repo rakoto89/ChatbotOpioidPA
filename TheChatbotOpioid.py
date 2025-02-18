785bffc2664b15dd543cc44d4accc179ae537846
rakoto89 <121390005+rakoto89@users.noreply.github.com>
2025-02-17 19:47:30 -0500

    Update TheChatbotOpioid.py

--- TheChatbotOpioid.py
+++ TheChatbotOpioid.py
@@ -94,4 +94,4 @@ def ask():
 application = app  
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
